64ab20bce6efb6ef680f4356e527c39a9b22416b
LuoSheng12345 <86453390+LuoSheng12345@users.noreply.github.com>
2021-07-07 07:33:36 +1000

    new converter

--- json/jsonConverter.js
+++ json/jsonConverter.js
@@ -2,128 +2,68 @@ const fs=require('fs')
 const yaml = require('js-yaml');
 
 
-const basePath='../data'
-const familyPath=basePath+'/family'
-const personPath=basePath+'/person'
-const companyPath=basePath+'/company'
-
-var nodes=[]
-
-var categories=[{
-    cid:0,
-    name:'公司或集团'
-}]
-
-//adding people to nodes
-var nid=0
-
-people=fs.readdirSync(personPath)
-
-for (x in people){
-    let doc=yaml.load(fs.readFileSync(personPath+'/'+people[x]+'/'+ 'brief.yaml'))
-    // console.log(doc)
-    nodes.push({
-        id:nid,
-        name:people[x],
-        isAlive:!doc.death,
-        info:{...doc},
-        connections:0
-    })
-    nid+=1
-}
-
-//add category to people
-families=fs.readdirSync(familyPath)
-
-var cidCounter=1
-
-for (f in families){
-    let doc=yaml.load(fs.readFileSync(familyPath+'/'+families[f]))
-    //make categories
-    categories.push({
-        cid:cidCounter,
-        name:doc.name
-    })
+const basePath='../数据/'
+const entitiesPath=basePath+'个体/'
+const relationsPath=basePath+'关系.yaml'
 
 
-    for(person in nodes){
-        if(nodes[person].name==doc.name){
-            nodes[person].category=cidCounter
-            nodes[person].isInner=true
-        }
-    }
+// var entities=fs.readdirSync(entitiesPath)
 
-    //loop through inner
-    if(doc.inner){
-        doc.inner.forEach(innerName => {
-            for(person in nodes){
-                if(nodes[person].name==innerName){
-                    if(!nodes[person].category){
-                        nodes[person].category=cidCounter
-                        nodes[person].isInner=true
-                    }
-                    break
-                }
-            }
-            
-        });
-    }
-    //loop through outer
-    if(doc.outer){
-        doc.outer.forEach(outerName => {
-            for(person in nodes){
-                if(nodes[person].name==outerName){
-                    if(!nodes[person].category){
-                        nodes[person].category=cidCounter
-                        nodes[person].isInner=false
+//the baseline is now relations. entities are now optional (or lets say an entity appears in the relations no longer require a dedicated yaml file)
+var relations=yaml.load(fs.readFileSync(relationsPath))
 
-                    }
-                    break
-                }
-            }
-            
-        });
-    }
-
-    //end. Update cidCounter
-    cidCounter+=1
-}
-
-
-//add companies to nodes
-companies=fs.readdirSync(companyPath)
-
-for (x in companies){
-    nodes.push({
-        id:nid,
-        name:companies[x],
-        category:0,
-        connections:0
-    })
-    nid+=1
-}
-
-//adding link
+var nodes={}
 var links=[]
 
-for (f in families){
-    let doc=yaml.load(fs.readFileSync(familyPath+'/'+families[f]))
-    let rel=doc.relations
-    rel.forEach(r=>{
-        let fromObj=nodes.filter(x=>x.name==r[0])[0]
-        let toObj=nodes.filter(x=>x.name==r[1])[0]
-        links.push({
-            source:fromObj.id,
-            target:toObj.id,
-            relationship:r[2]
-        })
-        nodes[fromObj.id].connections+=1
-        nodes[toObj.id].connections+=1
-    })
+function addNodeOrAddConnections(name){
+    if(!nodes[name]){
+        var ya=null
+        if(fs.existsSync(entitiesPath+name+'.yaml')){
+            var file=fs.readFileSync(entitiesPath+name+'.yaml',"utf-8")
+            ya=yaml.load(file)
+        }
+        nodes[name]={
+            name:name,
+            connections:1,
+            data:ya
+        }
+    }else{
+        nodes[name].connections+=1
+    }
 
+    // if(name=='邓子恢'){
+    //     console.log(nodes[name].connections)
+    // }
 }
 
+relations.forEach(r => {
+    var shui=r.谁
+    var shishui=r.是谁
 
+    if(shui.includes('邓子恢')||shishui.includes('邓子恢')){
+        console.log(r)
+    }
 
-var convertJson=JSON.stringify({nodes,links,categories})
-fs.writeFileSync('zhao.json',convertJson)
+    shui.forEach(sourceEntity=>{
+        addNodeOrAddConnections(sourceEntity)
+        shishui.forEach(targetEntity=>{
+            addNodeOrAddConnections(targetEntity)
+            links.push({
+                source:sourceEntity,
+                target:targetEntity,
+                value:r.的谁.join()
+            })
+        })
+    })
+    
+});
+
+// console.log(nodes)
+
+// console.log(links)
+
+const data=Object.values(nodes)
+
+var converted=JSON.stringify({data,links})
+
+fs.writeFileSync('zhao.json',converted)
